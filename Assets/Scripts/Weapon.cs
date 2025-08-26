181e1cc0440e38f8ffcafb884d37ccae808110ca
Tiomos <tiomos@live.com>
2025-08-26 17:18:25 +0200

    Add weapon animations and import third-party assets
    Added new weapon animation assets including idle and shoot states, and a weapon controller. Imported GDTV Sharp Shooter and StarterAssets third-party asset packs, including models, materials, prefabs, scripts, and scenes. Updated several SimpleFX material files.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] Animator animator;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int weaponDmg = 1;
     StarterAssetsInputs starterAssetsInputs;
+
+    const string SHOOT_STRING = "Shoot";
 
     void Awake()
     {
@@ -20,6 +25,10 @@ public class Weapon : MonoBehaviour
     {
         if (!starterAssetsInputs.shoot) return false;
 
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
@@ -28,7 +37,6 @@ public class Weapon : MonoBehaviour
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponDmg);
 
-            starterAssetsInputs.ShootInput(false);
         }
 
         return true;
